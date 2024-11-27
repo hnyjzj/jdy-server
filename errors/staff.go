32aacea800a1fc3be44bdfb82c23135ea4aa3b96
waset <waset@foxmail.com>
2024-11-27 17:21:57 +0800

    fix(errors/staff.go): 修正用户相关错误提示为账号

--- errors/staff.go
+++ errors/staff.go
@@ -1,10 +1,10 @@
 package errors
 
 var (
-	// 用户不存在
-	ErrStaffNotFound = New("用户不存在", C404)
-	// 用户已被禁用
-	ErrStaffDisabled = New("用户已被禁用", C403)
+	// 账号不存在
+	ErrStaffNotFound = New("账号不存在", C404)
+	// 账号已被禁用
+	ErrStaffDisabled = New("账号已被禁用", C403)
 	// 密码错误
 	ErrPasswordIncorrect = New("密码错误", C403)
 )

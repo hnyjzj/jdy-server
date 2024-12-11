2030138efcd7dbcd44d964b35057d5dcc30d4588
waset <waset@foxmail.com>
2024-12-11 09:55:43 +0800

    feat(product_param): 添加全部选项到产品参数枚举

--- types/product_param.go
+++ types/product_param.go
@@ -5,22 +5,24 @@ package types
 type ProductQuality int
 
 const (
-	ProductQuality99999 ProductQuality = iota + 1 // 999.99
-	ProductQuality9999                            // 999.9
-	ProductQuality999                             // 999
-	ProductQuality990                             // 990
-	ProductQuality950                             // 950
-	ProductQuality925                             // 925
-	ProductQuality916                             // 916
-	ProductQuality900                             // 900
-	ProductQuality750                             // 750
-	ProductQuality585                             // 585
-	ProductQuality375                             // 375
-	ProductQuality1                               // 1
-	ProductQualityGem                             // 裸石
+	ProductQualityAll   ProductQuality = iota // 全部
+	ProductQuality99999                       // 999.99
+	ProductQuality9999                        // 999.9
+	ProductQuality999                         // 999
+	ProductQuality990                         // 990
+	ProductQuality950                         // 950
+	ProductQuality925                         // 925
+	ProductQuality916                         // 916
+	ProductQuality900                         // 900
+	ProductQuality750                         // 750
+	ProductQuality585                         // 585
+	ProductQuality375                         // 375
+	ProductQuality1                           // 1
+	ProductQualityGem                         // 裸石
 )
 
 var ProductQualityMap = map[ProductQuality]string{
+	ProductQualityAll:   "全部",
 	ProductQuality99999: "999.99",
 	ProductQuality9999:  "999.9",
 	ProductQuality999:   "999",
@@ -41,17 +43,19 @@ var ProductQualityMap = map[ProductQuality]string{
 type ProductBrand int
 
 const (
-	ProductBrandJMF  ProductBrand = iota + 1 // 金美福
-	ProductBrandZDS                          // 周大生
-	ProductBrandLM                           // 老庙
-	ProductBrandZLF                          // 周六福
-	ProductBrandJDF                          // 金大福
-	ProductBrandCHJ                          // 潮宏基
-	ProductBrandZGJB                         // 中国珠宝
-	ProductBrandLMTG                         // 老庙推广
+	ProductBrandAll  ProductBrand = iota // 全部
+	ProductBrandJMF                      // 金美福
+	ProductBrandZDS                      // 周大生
+	ProductBrandLM                       // 老庙
+	ProductBrandZLF                      // 周六福
+	ProductBrandJDF                      // 金大福
+	ProductBrandCHJ                      // 潮宏基
+	ProductBrandZGJB                     // 中国珠宝
+	ProductBrandLMTG                     // 老庙推广
 )
 
 var ProductBrandMap = map[ProductBrand]string{
+	ProductBrandAll:  "全部",
 	ProductBrandJMF:  "金美福",
 	ProductBrandZDS:  "周大生",
 	ProductBrandLM:   "老庙",
@@ -67,27 +71,29 @@ var ProductBrandMap = map[ProductBrand]string{
 type ProductGem int
 
 const (
-	ProductGemGold        ProductGem = iota + 1 // 素金
-	ProductGemDiamond                           // 钻石
-	ProductGemSapphire                          // 蓝宝石
-	ProductGemTurquoise                         // 碧玺
-	ProductGemPearl                             // 珍珠
-	ProductGemJade                              // 翡翠
-	ProductGemJadeite                           // 和田玉
-	ProductGemCoral                             // 彩宝
-	ProductGemOther                             // 其他
-	ProductGemRuby                              // 红宝石
-	ProductGemCrystal                           // 水晶
-	ProductGemEmerald                           // 祖母绿
-	ProductGemOpal                              // 玉髓
-	ProductGemJasper                            // 玛瑙
-	ProductGemGarnet                            // 石榴石
-	ProductGemZircon                            // 锆石
-	ProductGemMalachite                         // 孔雀石
-	ProductGemPearlMother                       // 贝母
+	ProductGemAll         ProductGem = iota // 全部
+	ProductGemGold                          // 素金
+	ProductGemDiamond                       // 钻石
+	ProductGemSapphire                      // 蓝宝石
+	ProductGemTurquoise                     // 碧玺
+	ProductGemPearl                         // 珍珠
+	ProductGemJade                          // 翡翠
+	ProductGemJadeite                       // 和田玉
+	ProductGemCoral                         // 彩宝
+	ProductGemOther                         // 其他
+	ProductGemRuby                          // 红宝石
+	ProductGemCrystal                       // 水晶
+	ProductGemEmerald                       // 祖母绿
+	ProductGemOpal                          // 玉髓
+	ProductGemJasper                        // 玛瑙
+	ProductGemGarnet                        // 石榴石
+	ProductGemZircon                        // 锆石
+	ProductGemMalachite                     // 孔雀石
+	ProductGemPearlMother                   // 贝母
 )
 
 var ProductGemMap = map[ProductGem]string{
+	ProductGemAll:         "全部",
 	ProductGemGold:        "素金",
 	ProductGemDiamond:     "钻石",
 	ProductGemSapphire:    "蓝宝石",
@@ -113,14 +119,16 @@ var ProductGemMap = map[ProductGem]string{
 type ProductSupplier int
 
 const (
-	ProductSupplierJMF ProductSupplier = iota + 1 // 金美福
-	ProductSupplierZDS                            // 周大生
-	ProductSupplierLM                             // 老庙
-	ProductSupplierZLF                            // 周六福
-	ProductSupplierJDF                            // 金大福
+	ProductSupplierAll ProductSupplier = iota // 全部
+	ProductSupplierJMF                        // 金美福
+	ProductSupplierZDS                        // 周大生
+	ProductSupplierLM                         // 老庙
+	ProductSupplierZLF                        // 周六福
+	ProductSupplierJDF                        // 金大福
 )
 
 var ProductSupplierMap = map[ProductSupplier]string{
+	ProductSupplierAll: "全部",
 	ProductSupplierJMF: "金美福",
 	ProductSupplierZDS: "周大生",
 	ProductSupplierLM:  "老庙",
@@ -133,24 +141,26 @@ var ProductSupplierMap = map[ProductSupplier]string{
 type ProductCategory int
 
 const (
-	ProductCategoryRing      ProductCategory = iota + 1 // 戒指
-	ProductCategoryNecklace                             // 项链
-	ProductCategoryChoker                               // 套链
-	ProductCategoryPendant                              // 吊坠
-	ProductCategoryEarring                              // 耳饰
-	ProductCategoryBracelet                             // 手链
-	ProductCategoryBangle                               // 手镯
-	ProductCategoryAnklet                               // 脚链
-	ProductCategoryBrooch                               // 胸针
-	ProductCategoryBead                                 // 珠串
-	ProductCategoryAccessory                            // 挂件
-	ProductCategoryGoldBar                              // 金条
-	ProductCategoryOrnament                             // 摆件
-	ProductCategoryJewelry                              // 饰品
-	ProductCategoryOther                                // 其他
+	ProductCategoryAll       ProductCategory = iota // 全部
+	ProductCategoryRing                             // 戒指
+	ProductCategoryNecklace                         // 项链
+	ProductCategoryChoker                           // 套链
+	ProductCategoryPendant                          // 吊坠
+	ProductCategoryEarring                          // 耳饰
+	ProductCategoryBracelet                         // 手链
+	ProductCategoryBangle                           // 手镯
+	ProductCategoryAnklet                           // 脚链
+	ProductCategoryBrooch                           // 胸针
+	ProductCategoryBead                             // 珠串
+	ProductCategoryAccessory                        // 挂件
+	ProductCategoryGoldBar                          // 金条
+	ProductCategoryOrnament                         // 摆件
+	ProductCategoryJewelry                          // 饰品
+	ProductCategoryOther                            // 其他
 )
 
 var ProductCategoryMap = map[ProductCategory]string{
+	ProductCategoryAll:       "全部",
 	ProductCategoryRing:      "戒指",
 	ProductCategoryNecklace:  "项链",
 	ProductCategoryChoker:    "套链",
@@ -173,22 +183,24 @@ var ProductCategoryMap = map[ProductCategory]string{
 type ProductCraft int
 
 const (
-	ProductCraftNone     ProductCraft = iota + 1 // 无
-	ProductCraft3D                               // 3D
-	ProductCraft5D                               // 5D
-	ProductCraft5G                               // 5G
-	ProductCraftAncient                          // 古法
-	ProductCraftRetro                            // 复古
-	ProductCraftFiveGold                         // 万足金
-	ProductCraftNineGold                         // 九五金
-	ProductCraftFengLan                          // 珐琅彩
-	ProductCraftFineA                            // 精品A
-	ProductCraftFineB                            // 精品B
-	ProductCraftFineC                            // 精品C
-	ProductCraftFineD                            // 精品D
+	ProductCraftAll      ProductCraft = iota // 全部
+	ProductCraftNone                         // 无
+	ProductCraft3D                           // 3D
+	ProductCraft5D                           // 5D
+	ProductCraft5G                           // 5G
+	ProductCraftAncient                      // 古法
+	ProductCraftRetro                        // 复古
+	ProductCraftFiveGold                     // 万足金
+	ProductCraftNineGold                     // 九五金
+	ProductCraftFengLan                      // 珐琅彩
+	ProductCraftFineA                        // 精品A
+	ProductCraftFineB                        // 精品B
+	ProductCraftFineC                        // 精品C
+	ProductCraftFineD                        // 精品D
 )
 
 var ProductCraftMap = map[ProductCraft]string{
+	ProductCraftAll:      "全部",
 	ProductCraftNone:     "无",
 	ProductCraft3D:       "3D",
 	ProductCraft5D:       "5D",
@@ -209,41 +221,43 @@ var ProductCraftMap = map[ProductCraft]string{
 type ProductColor int
 
 const (
-	ProductColorD  ProductColor = iota + 1 // D
-	ProductColorE                          // E
-	ProductColorDE                         // D-E
-	ProductColorF                          // F
-	ProductColorG                          // G
-	ProductColorFG                         // F-G
-	ProductColorH                          // H
-	ProductColorI                          // I
-	ProductColorJ                          // J
-	ProductColorIJ                         // I-J
-	ProductColorK                          // K
-	ProductColorL                          // L
-	ProductColorKL                         // K-L
-	ProductColorM                          // M
-	ProductColorN                          // N
-	ProductColorMN                         // M-N
+	ProductColorAll ProductColor = iota // 全部
+	ProductColorD                       // D
+	ProductColorE                       // E
+	ProductColorDE                      // D-E
+	ProductColorF                       // F
+	ProductColorG                       // G
+	ProductColorFG                      // F-G
+	ProductColorH                       // H
+	ProductColorI                       // I
+	ProductColorJ                       // J
+	ProductColorIJ                      // I-J
+	ProductColorK                       // K
+	ProductColorL                       // L
+	ProductColorKL                      // K-L
+	ProductColorM                       // M
+	ProductColorN                       // N
+	ProductColorMN                      // M-N
 )
 
 var ProductColorMap = map[ProductColor]string{
-	ProductColorD:  "D",
-	ProductColorE:  "E",
-	ProductColorDE: "D-E",
-	ProductColorF:  "F",
-	ProductColorG:  "G",
-	ProductColorFG: "F-G",
-	ProductColorH:  "H",
-	ProductColorI:  "I",
-	ProductColorJ:  "J",
-	ProductColorIJ: "I-J",
-	ProductColorK:  "K",
-	ProductColorL:  "L",
-	ProductColorKL: "K-L",
-	ProductColorM:  "M",
-	ProductColorN:  "N",
-	ProductColorMN: "M-N",
+	ProductColorAll: "全部",
+	ProductColorD:   "D",
+	ProductColorE:   "E",
+	ProductColorDE:  "D-E",
+	ProductColorF:   "F",
+	ProductColorG:   "G",
+	ProductColorFG:  "F-G",
+	ProductColorH:   "H",
+	ProductColorI:   "I",
+	ProductColorJ:   "J",
+	ProductColorIJ:  "I-J",
+	ProductColorK:   "K",
+	ProductColorL:   "L",
+	ProductColorKL:  "K-L",
+	ProductColorM:   "M",
+	ProductColorN:   "N",
+	ProductColorMN:  "M-N",
 }
 
 /* 产品净度 */
@@ -251,23 +265,25 @@ var ProductColorMap = map[ProductColor]string{
 type ProductClarity int
 
 const (
-	ProductClarityIF   ProductClarity = iota + 1 // IF
-	ProductClarityVVS                            // VVS
-	ProductClarityVVS1                           // VVS1
-	ProductClarityVVS2                           // VVS2
-	ProductClarityVS                             // VS
-	ProductClarityVS1                            // VS1
-	ProductClarityVS2                            // VS2
-	ProductClaritySI                             // SI
-	ProductClaritySI1                            // SI1
-	ProductClaritySI2                            // SI2
-	ProductClaritySI3                            // SI3
-	ProductClarityP                              // P
-	ProductClarityFL                             // FL
-	ProductClarityLC                             // LC
+	ProductClarityAll  ProductClarity = iota // 全部
+	ProductClarityIF                         // IF
+	ProductClarityVVS                        // VVS
+	ProductClarityVVS1                       // VVS1
+	ProductClarityVVS2                       // VVS2
+	ProductClarityVS                         // VS
+	ProductClarityVS1                        // VS1
+	ProductClarityVS2                        // VS2
+	ProductClaritySI                         // SI
+	ProductClaritySI1                        // SI1
+	ProductClaritySI2                        // SI2
+	ProductClaritySI3                        // SI3
+	ProductClarityP                          // P
+	ProductClarityFL                         // FL
+	ProductClarityLC                         // LC
 )
 
 var ProductClarityMap = map[ProductClarity]string{
+	ProductClarityAll:  "全部",
 	ProductClarityIF:   "IF",
 	ProductClarityVVS:  "VVS",
 	ProductClarityVVS1: "VVS1",
@@ -289,17 +305,19 @@ var ProductClarityMap = map[ProductClarity]string{
 type ProductCut int
 
 const (
-	ProductCutEX ProductCut = iota + 1 // EX
-	ProductCutVG                       // VG
-	ProductCutG                        // G
-	ProductCutP                        // P
+	ProductCutAll ProductCut = iota // 全部
+	ProductCutEX                    // EX
+	ProductCutVG                    // VG
+	ProductCutG                     // G
+	ProductCutP                     // P
 )
 
 var ProductCutMap = map[ProductCut]string{
-	ProductCutEX: "EX",
-	ProductCutVG: "VG",
-	ProductCutG:  "G",
-	ProductCutP:  "P",
+	ProductCutAll: "全部",
+	ProductCutEX:  "EX",
+	ProductCutVG:  "VG",
+	ProductCutG:   "G",
+	ProductCutP:   "P",
 }
 
 /* 产品材质 */
@@ -307,14 +325,16 @@ var ProductCutMap = map[ProductCut]string{
 type ProductMaterial int
 
 const (
-	ProductMaterialGold      ProductMaterial = iota + 1 // 黄金
-	ProductMaterialSilver                               // 银饰
-	ProductMaterialPlatinum                             // 铂金
-	ProductMaterialPalladium                            // 钯金
-	ProductMaterialGem                                  // 裸石
+	ProductMaterialAll       ProductMaterial = iota // 全部
+	ProductMaterialGold                             // 黄金
+	ProductMaterialSilver                           // 银饰
+	ProductMaterialPlatinum                         // 铂金
+	ProductMaterialPalladium                        // 钯金
+	ProductMaterialGem                              // 裸石
 )
 
 var ProductMaterialMap = map[ProductMaterial]string{
+	ProductMaterialAll:       "全部",
 	ProductMaterialGold:      "黄金",
 	ProductMaterialSilver:    "银饰",
 	ProductMaterialPlatinum:  "铂金",
@@ -327,22 +347,24 @@ var ProductMaterialMap = map[ProductMaterial]string{
 type ProductClass int
 
 const (
-	ProductClassGoldKg    ProductClass = iota + 1 // 足金（克）
-	ProductClassGoldPiece                         // 足金（件）
-	ProductClassGold750                           // 金 750
-	ProductClassGold916                           // 金 916
-	ProductClassPlatinum                          // 铂金
-	ProductClassSilver                            // 银饰
-	ProductClassGoldInlay                         // 足金镶嵌
-	ProductClassGem                               // 裸石
-	ProductClassDiamond                           // 钻石
-	ProductClassJade                              // 玉石
-	ProductClassPearl                             // 珍珠
-	ProductClassOther                             // 其他
+	ProductClassAll       ProductClass = iota // 全部
+	ProductClassGoldKg                        // 足金（克）
+	ProductClassGoldPiece                     // 足金（件）
+	ProductClassGold750                       // 金 750
+	ProductClassGold916                       // 金 916
+	ProductClassPlatinum                      // 铂金
+	ProductClassSilver                        // 银饰
+	ProductClassGoldInlay                     // 足金镶嵌
+	ProductClassGem                           // 裸石
+	ProductClassDiamond                       // 钻石
+	ProductClassJade                          // 玉石
+	ProductClassPearl                         // 珍珠
+	ProductClassOther                         // 其他
 
 )
 
 var ProductClassMap = map[ProductClass]string{
+	ProductClassAll:       "全部",
 	ProductClassGoldKg:    "足金（克）",
 	ProductClassGoldPiece: "足金（件）",
 	ProductClassGold750:   "金 750",
@@ -362,12 +384,14 @@ var ProductClassMap = map[ProductClass]string{
 type ProductRetailType int
 
 const (
-	ProductRetailTypePiece     ProductRetailType = iota + 1 // 计件
-	ProductRetailTypeGoldKg                                 // 计重工费按克
-	ProductRetailTypeGoldPiece                              // 计重工费按件
+	ProductRetailTypeAll       ProductRetailType = iota // 全部
+	ProductRetailTypePiece                              // 计件
+	ProductRetailTypeGoldKg                             // 计重工费按克
+	ProductRetailTypeGoldPiece                          // 计重工费按件
 )
 
 var ProductRetailTypeMap = map[ProductRetailType]string{
+	ProductRetailTypeAll:       "全部",
 	ProductRetailTypePiece:     "计件",
 	ProductRetailTypeGoldKg:    "计重工费按克",
 	ProductRetailTypeGoldPiece: "计重工费按件",
@@ -378,12 +402,14 @@ var ProductRetailTypeMap = map[ProductRetailType]string{
 type ProductStatus int
 
 const (
-	ProductStatusInStock  ProductStatus = iota + 1 // 在库
-	ProductStatusInRepair                          // 维修中
-	ProductStatusOutStock                          // 调出在途
+	ProductStatusAll      ProductStatus = iota // 全部
+	ProductStatusInStock                       // 在库
+	ProductStatusInRepair                      // 维修中
+	ProductStatusOutStock                      // 调出在途
 )
 
 var ProductStatusMap = map[ProductStatus]string{
+	ProductStatusAll:      "全部",
 	ProductStatusInStock:  "在库",
 	ProductStatusInRepair: "维修中",
 	ProductStatusOutStock: "调出在途",

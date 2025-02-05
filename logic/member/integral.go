8c08bc82a91b6fbf0f34c72cb41c39c5e889c98f
waset <waset@foxmail.com>
2025-02-05 15:18:45 +0800

    fix(member): 优化积分变更逻辑及格式化员工信息

--- logic/member/integral.go
+++ logic/member/integral.go
@@ -26,14 +26,14 @@ func (l *MemberIntegralLogic) List(req *types.MemberIntegralListReq) (*types.Pag
 
 	// 获取总数
 	if err := db.Count(&res.Total).Error; err != nil {
-		return nil, errors.New("获取列表失败: " + err.Error())
+		return nil, errors.New("获取总数失败")
 	}
 
 	// 获取列表
 	db = db.Order("created_at desc")
 	db = model.PageCondition(db, req.Page, req.Limit)
 	if err := db.Find(&res.List).Error; err != nil {
-		return nil, errors.New("获取列表失败: " + err.Error())
+		return nil, errors.New("获取列表失败")
 	}
 
 	return &res, nil
@@ -45,13 +45,13 @@ func (l *MemberIntegralLogic) Change(req *types.MemberIntegralChangeReq) error {
 	)
 
 	if err := model.DB.Model(&member).Where("id = ?", req.MemberId).First(&member).Error; err != nil {
-		return errors.New("获取会员失败: " + err.Error())
+		return errors.New("获取会员失败")
 	}
 
-	reason := fmt.Sprintf("员工 %s 调整：%s", l.Staff.Phone, req.Reason)
+	reason := fmt.Sprintf("员工【%s】：%s", l.Staff.Phone, req.Reason)
 
 	if err := member.IntegralChange(model.DB, req.Change, enums.MemberIntegralChangeTypeAdjust, reason); err != nil {
-		return errors.New("积分变更失败: " + err.Error())
+		return errors.New("积分变更失败")
 	}
 
 	return nil

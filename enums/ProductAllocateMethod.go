0a0e0e4c73a2b5468c1144b576605b721b8e4c3c
waset <waset@foxmail.com>
2025-02-21 09:22:22 +0800

    feat(enums): 添加产品调拨方式“全部”选项

--- enums/ProductAllocateMethod.go
+++ enums/ProductAllocateMethod.go
@@ -3,15 +3,17 @@ package enums
 import "errors"
 
 /* 调拨方式 */
-// 门店调拨、调拨出库
+// 全部、门店调拨、调拨出库
 type ProductAllocateMethod int
 
 const (
-	ProductAllocateMethodStore ProductAllocateMethod = iota + 1 // 门店调拨
-	ProductAllocateMethodOut                                    // 调拨出库
+	ProductAllocateMethodAll   ProductAllocateMethod = iota // 全部
+	ProductAllocateMethodStore                              // 门店调拨
+	ProductAllocateMethodOut                                // 调拨出库
 )
 
 var ProductAllocateMethodMap = map[ProductAllocateMethod]string{
+	ProductAllocateMethodAll:   "全部",
 	ProductAllocateMethodStore: "门店调拨",
 	ProductAllocateMethodOut:   "调拨出库",
 }

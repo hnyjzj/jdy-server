a0ad3c312abd602b16b2f395dbf290c43a82e08a
waset <waset@foxmail.com>
2025-02-28 14:41:12 +0800

    fix: 修改统计接口参数验证方式

--- types/statistic.go
+++ types/statistic.go
@@ -3,13 +3,13 @@ package types
 import "jdy/enums"
 
 type StatisticStoreSalesTotalReq struct {
-	Duration enums.Duration `json:"duration" required:"true"`
+	Duration enums.Duration `json:"duration" binding:"required"`
 }
 
 type StatisticTodaySalesReq struct {
-	StoreId string `json:"store_id" required:"true"`
+	StoreId string `json:"store_id" binding:"required"`
 }
 
 type StatisticTodayProductReq struct {
-	StoreId string `json:"store_id" required:"true"`
+	StoreId string `json:"store_id" binding:"required"`
 }

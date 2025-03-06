d9eebe0da1a54b67e1b7bbdf9b754c1282a2f693
waset <waset@foxmail.com>
2025-03-06 10:26:59 +0800

    fix(gold_price): 添加门店无接收者时的日志记录并优化消息发送逻辑

--- service/crons/gold_price.go
+++ service/crons/gold_price.go
@@ -6,6 +6,7 @@ import (
 	"jdy/enums"
 	"jdy/message"
 	"jdy/model"
+	"log"
 
 	"gorm.io/gorm"
 )
@@ -31,11 +32,16 @@ func SendGoldPriceSetMessage() {
 					receiver = append(receiver, *staff.Account.Username)
 				}
 			}
+			if len(receiver) == 0 {
+				log.Printf("门店 %s 没有有效的接收者，跳过消息发送", v.Name)
+				continue
+			}
 			m := message.NewMessage(context.Background())
 			m.SendGoldPriceSetMessage(&message.GoldPriceMessage{
 				ToUser:    receiver,
 				StoreName: v.Name,
 			})
+			log.Printf("成功向门店 %s 的 %d 位员工发送金价设置提醒", v.Name, len(receiver))
 		}
 	}
 }

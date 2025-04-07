bc8362d91c2ab42efb21ac26192bd16c08d453e2
waset <waset@foxmail.com>
2025-04-07 13:08:25 +0800

    feat(product): 增加旧料转成品逻辑

--- logic/product/old.go
+++ logic/product/old.go
@@ -1,6 +1,8 @@
 package product
 
 import (
+	"database/sql"
+	"jdy/enums"
 	"jdy/errors"
 	"jdy/model"
 	"jdy/types"
@@ -61,14 +63,38 @@ func (l *ProductOldLogic) Conversion(req *types.ProductConversionReq) *errors.Er
 	// 开启事务
 	if err := model.DB.Transaction(func(tx *gorm.DB) error {
 		// 查询商品信息
-		var product model.ProductOld
-		if err := tx.Unscoped().Where("id = ?", req.Id).First(&product).Error; err != nil {
+		var old_product model.ProductOld
+		if err := tx.Unscoped().Where("id = ?", req.Id).First(&old_product).Error; err != nil {
 			return errors.New("商品不存在")
 		}
 
 		// 判断旧料状态
-		if !product.IsOur {
+		if !old_product.IsOur {
 			return errors.New("非自有旧料无法转换")
+		}
+
+		// 转换
+		var finished_product model.ProductFinished
+		if err := tx.Unscoped().Where("code = ?", old_product.Code).First(&finished_product).Error; err != nil {
+			return errors.New("成品不在库中")
+		}
+		if finished_product.Status != enums.ProductStatusDamage && finished_product.Status != enums.ProductStatusSold { // 判断成品状态
+			return errors.New("成品不在库中")
+		}
+		// 更新成品状态,如果被删除了，则恢复
+		if finished_product.DeletedAt.Valid {
+			if err := tx.Model(&finished_product).Update("deleted_at", sql.NullTime{}).Error; err != nil {
+				return errors.New("恢复成品失败")
+			}
+		}
+		finished_product.Status = enums.ProductStatusNormal
+		if err := tx.Save(&finished_product).Error; err != nil {
+			return errors.New("更新成品失败")
+		}
+
+		// 删除旧料
+		if err := tx.Where("id = ?", old_product.Id).Delete(&old_product).Error; err != nil {
+			return errors.New("删除旧料失败")
 		}
 
 		return nil

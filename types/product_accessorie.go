9714983b900a8c086c77ae45ec441dfe977229d0
waset <waset@foxmail.com>
2025-04-11 14:12:41 +0800

    fix(accessorie): 修改入网费类型为指针

--- types/product_accessorie.go
+++ types/product_accessorie.go
@@ -6,12 +6,12 @@ import (
 
 // 配件条件
 type ProductAccessorieWhere struct {
-	StoreId   string          `json:"store_id" label:"门店" find:"true" create:"true" update:"false" sort:"1" type:"string" input:"text" required:"true"`      // 门店
-	EntryId   string          `json:"entry_id" label:"入库单" find:"true" create:"true" update:"false" sort:"2" type:"string" input:"text" required:"true"`     // 入库单
-	Code      string          `json:"code" label:"类目" find:"true" create:"true" update:"false" sort:"3" type:"string" input:"text" required:"true"`          // 类目
-	Name      string          `json:"name" label:"名称" find:"true" create:"true" update:"false" sort:"4" type:"string" input:"text" required:"true"`          // 名称
-	Stock     int64           `json:"stock" label:"库存" find:"false" create:"true" update:"false" sort:"5" type:"number" input:"number" required:"true"`      // 库存
-	AccessFee decimal.Decimal `json:"access_fee" label:"入网费" find:"false" create:"true" update:"false" sort:"6" type:"float" input:"number" required:"true"` // 入网费
+	StoreId   string           `json:"store_id" label:"门店" find:"true" create:"true" update:"false" sort:"1" type:"string" input:"text" required:"true"`      // 门店
+	EntryId   string           `json:"entry_id" label:"入库单" find:"true" create:"true" update:"false" sort:"2" type:"string" input:"text" required:"true"`     // 入库单
+	Code      string           `json:"code" label:"类目" find:"true" create:"true" update:"false" sort:"3" type:"string" input:"text" required:"true"`          // 类目
+	Name      string           `json:"name" label:"名称" find:"true" create:"true" update:"false" sort:"4" type:"string" input:"text" required:"true"`          // 名称
+	Stock     int64            `json:"stock" label:"库存" find:"false" create:"true" update:"false" sort:"5" type:"number" input:"number" required:"true"`      // 库存
+	AccessFee *decimal.Decimal `json:"access_fee" label:"入网费" find:"false" create:"true" update:"false" sort:"6" type:"float" input:"number" required:"true"` // 入网费
 }
 
 // 配件列表

2324049d8defbbaf21c57d731856f3fd93356282
waset <waset@foxmail.com>
2025-04-15 11:38:05 +0800

    feat(enums): 更新产品品牌和供应商枚举值

--- enums/ProductBrand.go
+++ enums/ProductBrand.go
@@ -3,29 +3,37 @@ package enums
 import "errors"
 
 /* 产品品牌 */
-// 金美福、周大生、老庙、周六福、金大福、潮宏基、中国珠宝、老庙推广
+// 金美福、周大生、老庙、周六福、金大福、中国珠宝、老庙推广价、金大福推广价、金六福推广价、金美福推广价、中国珠宝推广价、潮宏基
 type ProductBrand int
 
 const (
-	ProductBrandJMF  ProductBrand = iota + 1 // 金美福
-	ProductBrandZDS                          // 周大生
-	ProductBrandLM                           // 老庙
-	ProductBrandZLF                          // 周六福
-	ProductBrandJDF                          // 金大福
-	ProductBrandCHJ                          // 潮宏基
-	ProductBrandZGJB                         // 中国珠宝
-	ProductBrandLMTG                         // 老庙推广
+	ProductBrandJMF   ProductBrand = iota + 1 // 金美福
+	ProductBrandZDS                           // 周大生
+	ProductBrandLM                            // 老庙
+	ProductBrandZLF                           // 周六福
+	ProductBrandJDF                           // 金大福
+	ProductBrandZJ                            // 中国珠宝
+	ProductBrandLMTP                          // 老庙推广价
+	ProductBrandJDFTP                         // 金大福推广价
+	ProductBrandJLFTP                         // 金六福推广价
+	ProductBrandJMFTP                         // 金美福推广价
+	ProductBrandZJTP                          // 中国珠宝推广价
+	ProductBrandCHJ                           // 潮宏基
 )
 
 var ProductBrandMap = map[ProductBrand]string{
-	ProductBrandJMF:  "金美福",
-	ProductBrandZDS:  "周大生",
-	ProductBrandLM:   "老庙",
-	ProductBrandZLF:  "周六福",
-	ProductBrandJDF:  "金大福",
-	ProductBrandCHJ:  "潮宏基",
-	ProductBrandZGJB: "中国珠宝",
-	ProductBrandLMTG: "老庙推广",
+	ProductBrandJMF:   "金美福",
+	ProductBrandZDS:   "周大生",
+	ProductBrandLM:    "老庙",
+	ProductBrandZLF:   "周六福",
+	ProductBrandJDF:   "金大福",
+	ProductBrandZJ:    "中国珠宝",
+	ProductBrandLMTP:  "老庙推广价",
+	ProductBrandJDFTP: "金大福推广价",
+	ProductBrandJLFTP: "金六福推广价",
+	ProductBrandJMFTP: "金美福推广价",
+	ProductBrandZJTP:  "中国珠宝推广价",
+	ProductBrandCHJ:   "潮宏基",
 }
 
 func (p ProductBrand) ToMap() any {
--- enums/ProductSupplier.go
+++ enums/ProductSupplier.go
@@ -3,25 +3,21 @@ package enums
 import "errors"
 
 /* 供应商 */
-// 金美福、周大生、老庙、潮宏基、金大福、周六福
+// 金美福、周大生、老庙黄金、郑州鑫美福
 type ProductSupplier int
 
 const (
-	ProductSupplierJMF ProductSupplier = iota + 1 // 金美福
-	ProductSupplierZDS                            // 周大生
-	ProductSupplierLMG                            // 老庙
-	ProductSupplierCHJ                            // 潮宏基
-	ProductSupplierJDF                            // 金大福
-	ProductSupplierZLF                            // 周六福
+	ProductSupplierJMF  ProductSupplier = iota + 1 // 金美福
+	ProductSupplierZDS                             // 周大生
+	ProductSupplierLMG                             // 老庙黄金
+	ProductSupplierZMXF                            // 郑州鑫美福
 )
 
 var ProductSupplierMap = map[ProductSupplier]string{
-	ProductSupplierJMF: "金美福",
-	ProductSupplierZDS: "周大生",
-	ProductSupplierLMG: "老庙",
-	ProductSupplierCHJ: "潮宏基",
-	ProductSupplierJDF: "金大福",
-	ProductSupplierZLF: "周六福",
+	ProductSupplierJMF:  "金美福",
+	ProductSupplierZDS:  "周大生",
+	ProductSupplierLMG:  "老庙黄金",
+	ProductSupplierZMXF: "郑州鑫美福",
 }
 
 func (p ProductSupplier) ToMap() any {

f7ac120bce780bdfa3503e0a8efb60a1cc2115b1
waset <waset@foxmail.com>
2025-06-14 16:11:04 +0800

    feat: 添加Staff模型字段注释及关联调整

--- model/staff.go
+++ model/staff.go
@@ -12,20 +12,20 @@ import (
 type Staff struct {
 	SoftDelete
 
-	Phone *string `json:"phone" gorm:"uniqueIndex;size:11;comment:手机号"`
+	Phone *string `json:"phone" gorm:"uniqueIndex;size:11;comment:手机号"` // 手机号
 
-	Nickname string       `json:"nickname" gorm:"column:nickname;index;comment:姓名"`
-	Avatar   string       `json:"avatar" gorm:"size:255;comment:头像"`
-	Email    string       `json:"email" gorm:"index;comment:邮箱"`
+	Nickname string       `json:"nickname" gorm:"column:nickname;index;comment:姓名"`        // 姓名
+	Avatar   string       `json:"avatar" gorm:"size:255;comment:头像"`                       // 头像
+	Email    string       `json:"email" gorm:"index;comment:邮箱"`                           // 邮箱
 	Gender   enums.Gender `json:"gender" gorm:"column:gender;type:tinyint(1);comment:性别;"` // 性别
 
-	IsDisabled bool `json:"is_disabled" gorm:"comment:是否禁用"`
+	IsDisabled bool `json:"is_disabled" gorm:"comment:是否禁用"` // 是否禁用
 
-	Account  *Account  `json:"account,omitempty" gorm:"foreignKey:StaffId;references:Id;"`
-	Accounts []Account `json:"accounts,omitempty" gorm:"foreignKey:StaffId;references:Id;"`
+	Account  *Account  `json:"account,omitempty" gorm:"foreignKey:StaffId;references:Id;"`  // 账号
+	Accounts []Account `json:"accounts,omitempty" gorm:"foreignKey:StaffId;references:Id;"` // 账号
 
-	Stores    []Store `json:"stores" gorm:"many2many:stores_staffs;"`
-	Superiors []Store `json:"superiors" gorm:"many2many:stores_superiors;"` // 负责人
+	Stores []Store `json:"stores" gorm:"many2many:staff_stores;"` // 店铺
+	Roles  []Role  `json:"roles" gorm:"many2many:staff_roles;"`   // 角色
 }
 
 func (Staff) WhereCondition(db *gorm.DB, query *types.StaffWhere) *gorm.DB {

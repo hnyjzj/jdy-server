ea63a366693dfbbe0425d84bfea255153df880c0
waset <waset@foxmail.com>
2025-07-22 09:38:02 +0800

    refactor(model/types): 移除ProductFinished和ProductFinishedWhere的All字段

--- model/product_finished.go
+++ model/product_finished.go
@@ -51,8 +51,6 @@ type ProductFinished struct {
 	EnterId   string                `json:"enter_id" gorm:"type:varchar(255);not NULL;comment:成品入库单ID;"`                    // 成品入库单ID
 	Enter     *ProductFinishedEnter `json:"product_enter,omitempty" gorm:"foreignKey:EnterId;references:Id;comment:成品入库单;"` // 成品入库单
 	EnterTime time.Time             `json:"enter_time" gorm:"comment:入库时间;"`                                                // 入库时间
-
-	All bool `json:"-" gorm:"-"`
 }
 
 func (ProductFinished) WhereCondition(db *gorm.DB, query *types.ProductFinishedWhere) *gorm.DB {
@@ -135,9 +133,6 @@ func (ProductFinished) WhereCondition(db *gorm.DB, query *types.ProductFinishedW
 	}
 	if query.EnterId != "" {
 		db = db.Where("enter_id = ?", query.EnterId) // 入库单ID
-	}
-	if query.All {
-		db = db.Unscoped()
 	}
 
 	return db

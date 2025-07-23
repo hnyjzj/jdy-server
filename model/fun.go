fc0311d3b46c104faa1bf5249829e80ec152d728
waset <waset@foxmail.com>
2025-07-23 18:01:24 +0800

    fix(model/fun.go): 修复时间范围查询跨天问题

--- model/fun.go
+++ model/fun.go
@@ -36,63 +36,70 @@ func DurationCondition(duration enums.Duration, fields ...string) func(tx *gorm.
 		case enums.DurationToday: // 今日
 			{
 				start := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
-				end := start.Add(24 * time.Hour)
+				end := start.Add(24*time.Hour - time.Nanosecond)
 
 				return tx.Where(fields[0]+" >= ? AND "+fields[0]+" < ?", start, end)
 			}
 		case enums.DurationYesterday: // 昨日
 			{
 				start := time.Date(now.Year(), now.Month(), now.Day()-1, 0, 0, 0, 0, now.Location())
-				end := start.Add(24 * time.Hour)
+				end := start.Add(24*time.Hour - time.Nanosecond)
 
 				return tx.Where(fields[0]+" >= ? AND "+fields[0]+" < ?", start, end)
 			}
 		case enums.DurationWeek: // 本周（周一为周起点）
 			{
 				start := time.Date(now.Year(), now.Month(), now.Day()-int(now.Weekday())+1, 0, 0, 0, 0, now.Location())
-				end := start.Add(7 * 24 * time.Hour)
+				end := start.Add(7*24*time.Hour - time.Nanosecond)
 
 				return tx.Where(fields[0]+" >= ? AND "+fields[0]+" < ?", start, end)
 			}
 		case enums.DurationLastWeek: // 上周
 			{
 				start := time.Date(now.Year(), now.Month(), now.Day()-int(now.Weekday())+1-7, 0, 0, 0, 0, now.Location())
-				end := start.Add(7 * 24 * time.Hour)
+				end := start.Add(7*24*time.Hour - time.Nanosecond)
 
 				return tx.Where(fields[0]+" >= ? AND "+fields[0]+" < ?", start, end)
 			}
 		case enums.DurationMonth: // 本月
 			{
 				start := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
-				end := start.Add(time.Date(now.Year(), now.Month()+1, 1, 0, 0, 0, 0, now.Location()).Sub(start))
+				end := start.Add(time.Date(now.Year(), now.Month()+1, 1, 0, 0, 0, 0, now.Location()).Sub(start)).Add(-time.Nanosecond)
 
 				return tx.Where(fields[0]+" >= ? AND "+fields[0]+" < ?", start, end)
 			}
 		case enums.DurationLastMonth: // 上月
 			{
 				start := time.Date(now.Year(), now.Month()-1, 1, 0, 0, 0, 0, now.Location())
-				end := start.Add(time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location()).Sub(start))
+				end := start.Add(time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location()).Sub(start)).Add(-time.Nanosecond)
 
 				return tx.Where(fields[0]+" >= ? AND "+fields[0]+" < ?", start, end)
 			}
 		case enums.DurationQuarter: // 本季度
 			{
 				start := time.Date(now.Year(), time.Month(int(now.Month())/3*3+1), 1, 0, 0, 0, 0, now.Location())
-				end := start.Add(time.Date(now.Year(), time.Month(int(now.Month())/3*3+4), 1, 0, 0, 0, 0, now.Location()).Sub(start))
+				end := start.Add(time.Date(now.Year(), time.Month(int(now.Month())/3*3+4), 1, 0, 0, 0, 0, now.Location()).Sub(start)).Add(-time.Nanosecond)
 
 				return tx.Where(fields[0]+" >= ? AND "+fields[0]+" < ?", start, end)
 			}
-		case enums.DurationYear: // 本年
+		case enums.DurationLastQuarter: // 上季度
+			{
+				start := time.Date(now.Year(), time.Month(int(now.Month())/3*3+1)-3, 1, 0, 0, 0, 0, now.Location())
+				end := start.Add(time.Date(now.Year(), time.Month(int(now.Month())/3*3+4)-3, 1, 0, 0, 0, 0, now.Location()).Sub(start)).Add(-time.Nanosecond)
+
+				return tx.Where(fields[0]+" >= ? AND "+fields[0]+" < ?", start, end)
+			}
+		case enums.DurationYear: // 今年
 			{
 				start := time.Date(now.Year(), 1, 1, 0, 0, 0, 0, now.Location())
-				end := start.Add(time.Date(now.Year()+1, 1, 1, 0, 0, 0, 0, now.Location()).Sub(start))
+				end := start.Add(time.Date(now.Year()+1, 1, 1, 0, 0, 0, 0, now.Location()).Sub(start)).Add(-time.Nanosecond)
 
 				return tx.Where(fields[0]+" >= ? AND "+fields[0]+" < ?", start, end)
 			}
 		case enums.DurationLastYear: // 去年
 			{
 				start := time.Date(now.Year()-1, 1, 1, 0, 0, 0, 0, now.Location())
-				end := start.Add(time.Date(now.Year(), 1, 1, 0, 0, 0, 0, now.Location()).Sub(start))
+				end := start.Add(time.Date(now.Year(), 1, 1, 0, 0, 0, 0, now.Location()).Sub(start)).Add(-time.Nanosecond)
 
 				return tx.Where(fields[0]+" >= ? AND "+fields[0]+" < ?", start, end)
 			}
@@ -103,13 +110,13 @@ func DurationCondition(duration enums.Duration, fields ...string) func(tx *gorm.
 					return tx
 				}
 
-				start, err := time.ParseInLocation("2006-01-02 15:04:05", fields[1], now.Location())
+				start, err := time.ParseInLocation(time.RFC3339, fields[1], now.Location())
 				if err != nil {
 					_ = tx.AddError(errors.New("start time format error"))
 					return tx
 				}
 
-				end, err := time.ParseInLocation("2006-01-02 15:04:05", fields[2], now.Location())
+				end, err := time.ParseInLocation(time.RFC3339, fields[2], now.Location())
 				if err != nil {
 					_ = tx.AddError(errors.New("end time format error"))
 					return tx

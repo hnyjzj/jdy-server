5e725578dd567bbb1c2d5f3f90526f89d362cec4
waset <waset@foxmail.com>
2025-07-23 09:52:29 +0800

    refactor(region): 优化区域列表默认值逻辑 (#80)

--- logic/region/region.go
+++ logic/region/region.go
@@ -67,11 +67,9 @@ func (l *RegionLogic) My(req *types.RegionListMyReq) (*[]model.Region, error) {
 		return nil, errors.New("获取区域列表失败")
 	}
 
-	if len(regions) >= 2 {
-		def := model.Region{}.Default(l.Staff.Identity)
-		if def != nil {
-			regions = append([]model.Region{*def}, regions...)
-		}
+	def := model.Region{}.Default(l.Staff.Identity)
+	if def != nil {
+		regions = append([]model.Region{*def}, regions...)
 	}
 
 	return &regions, nil

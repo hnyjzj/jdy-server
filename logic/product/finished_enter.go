41e4ca04d280fce3f0303d93d540e50a21d66870
waset <waset@foxmail.com>
2025-07-25 10:02:02 +0800

    fix(product): 优化入库单取消逻辑，增加状态和产品检查

--- logic/product/finished_enter.go
+++ logic/product/finished_enter.go
@@ -400,57 +400,20 @@ func (l *ProductFinishedEnterLogic) Cancel(req *types.ProductFinishedEnterCancel
 		return errors.New("入库单不存在")
 	}
 
+	if enter.Status != enums.ProductEnterStatusDraft {
+		return errors.New("入库单已结束，不允许操作")
+	}
+
+	if len(enter.Products) > 0 {
+		return errors.New("入库单存在产品，请清空后再操作")
+	}
+
 	if err := model.DB.Transaction(func(tx *gorm.DB) error {
-		switch enter.Status {
-		case enums.ProductEnterStatusDraft:
-			// 草稿直接取消
-			if err := tx.Model(&model.ProductFinishedEnter{}).Where("id = ?", enter.Id).Updates(model.ProductFinishedEnter{
-				Status: enums.ProductEnterStatusCanceled,
-			}).Error; err != nil {
-				return errors.New("入库单取消失败")
-			}
-
-		case enums.ProductEnterStatusCompleted:
-			// 已完成的入库单，需要将成品状态还原
-			for _, product := range enter.Products {
-				// 判断产品状态
-				if product.Status != enums.ProductStatusNormal {
-					return errors.New("成品状态不正确")
-				}
-
-				// 添加记录
-				history := model.ProductHistory{
-					Type:       enums.ProductTypeFinished,
-					OldValue:   product,
-					NewValue:   nil,
-					Action:     enums.ProductActionEntryCancel,
-					ProductId:  product.Id,
-					StoreId:    enter.StoreId,
-					SourceId:   enter.Id,
-					OperatorId: l.Staff.Id,
-					IP:         l.Ctx.ClientIP(),
-				}
-				if err := tx.Create(&history).Error; err != nil {
-					return errors.New("成品记录添加失败")
-				}
-
-				// 还原产品状态
-				if err := tx.Model(&model.ProductFinished{}).Where("id = ?", product.Id).Updates(model.ProductFinished{
-					Status: enums.ProductStatusDraft,
-				}).Error; err != nil {
-					return errors.New("成品状态还原失败")
-				}
-			}
-
-			// 更新入库单状态
-			if err := tx.Model(&model.ProductFinishedEnter{}).Where("id = ?", enter.Id).Updates(model.ProductFinishedEnter{
-				Status: enums.ProductEnterStatusCanceled,
-			}).Error; err != nil {
-				return errors.New("入库单取消失败")
-			}
-
-		default:
-			return errors.New("入库单状态不支持取消")
+		// 草稿直接取消
+		if err := tx.Model(&model.ProductFinishedEnter{}).Where("id = ?", enter.Id).Updates(model.ProductFinishedEnter{
+			Status: enums.ProductEnterStatusCanceled,
+		}).Error; err != nil {
+			return errors.New("入库单取消失败")
 		}
 
 		return nil

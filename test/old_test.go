847e83a98fee979f2b60387bcb187faa9832d16c
waset <waset@foxmail.com>
2025-08-18 15:48:36 +0800

    test: 添加产品旧数据编码生成测试 (#112)

---
+++ test/old_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"jdy/config"
+	"jdy/model"
+	"jdy/utils"
+	"strings"
+	"testing"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+	// 初始化配置
+	config.Init()
+	// 初始化数据库
+	model.Init()
+}
+
+func TestOldCreateCode(t *testing.T) {
+	var olds []model.ProductOld
+
+	if err := model.DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Unscoped().Where("code = '' or code is null or code = ' '").Find(&olds).Error; err != nil {
+			t.Error(err)
+			return err
+		}
+
+		for _, old := range olds {
+			if err := tx.Unscoped().Model(&model.ProductOld{}).Where("id = ?", old.Id).Update("code", strings.ToUpper("JL"+utils.RandomCode(8))).Error; err != nil {
+				t.Error(err)
+				return err
+			}
+		}
+
+		return nil
+	}); err != nil {
+		t.Error(err)
+	}
+}

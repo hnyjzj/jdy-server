648e8bff0606c4d2ce92e47e97021cbddd032fa9
waset <waset@foxmail.com>
2025-09-01 10:37:30 +0800

    refactor(order): 优化订单支付和退款处理逻辑 (#124)

--- enums/OrderType.go
+++ enums/OrderType.go
@@ -5,21 +5,23 @@ import (
 )
 
 /* 订单类型 */
-// 销售单、定金单、维修单、其他
+// 销售单、定金单、维修单、其他收支单、退货单
 type OrderType int
 
 const (
 	OrderTypeSales   OrderType = iota + 1 // 销售单
 	OrderTypeDeposit                      // 定金单
 	OrderTypeRepair                       // 维修单
-	OrderTypeOthers                       // 其他
+	OrderTypeOthers                       // 其他收支单
+	OrderTypeReturn                       // 退货单
 )
 
 var OrderTypeMap = map[OrderType]string{
 	OrderTypeSales:   "销售单",
 	OrderTypeDeposit: "定金单",
 	OrderTypeRepair:  "维修单",
-	OrderTypeOthers:  "其他",
+	OrderTypeOthers:  "其他收支单",
+	OrderTypeReturn:  "退货单",
 }
 
 func (p OrderType) ToMap() any {

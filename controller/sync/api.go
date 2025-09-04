13c4eed2ab85c17b47e14e7776cdcf6f76a330ee
waset <waset@foxmail.com>
2025-09-04 14:29:53 +0800

    refactor(sync): 重构API同步逻辑，优化代码结构

--- controller/sync/api.go
+++ controller/sync/api.go
@@ -1,13 +1,10 @@
 package sync
 
 import (
-	"jdy/model"
+	"jdy/logic/sync"
 	G "jdy/service/gin"
-	"log"
-	"strings"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
 type ApiController struct {
@@ -15,88 +12,18 @@ type ApiController struct {
 }
 
 func (con ApiController) List(ctx *gin.Context) {
-	routes := G.Gin.Routes()
+	var (
+		logic = sync.ApiLogic{}
+		req   = sync.ApiListReq{}
+	)
 
-	// 清空api表
-	if err := model.DB.Where("1 = 1").Delete(&model.Api{}).Error; err != nil {
-		log.Println(err)
+	req.Routes = G.Gin.Routes()
+	logic.Ctx = ctx
+
+	if err := logic.List(&req); err != nil {
+		con.Exception(ctx, err.Error())
 		return
 	}
 
-	for _, route := range routes {
-		names := strings.Split(route.Path, "/")
-		if names[1] != "api" {
-			continue
-		}
-
-		var (
-			parent *model.Api
-			err    error
-		)
-
-		paths := names[1:]
-		for i := range paths {
-			parentPaths := "/" + strings.Join(paths[:i], "/")
-			parentPaths = strings.TrimSpace(parentPaths)
-			parent, err = con.setParent(parentPaths, parent)
-			if err != nil {
-				log.Println(err)
-			}
-		}
-
-		var (
-			api model.Api
-		)
-		db := model.DB.Model(&api)
-		db = db.Unscoped()
-		db = db.Where(model.Api{
-			Path:   route.Path,
-			Method: route.Method,
-		})
-
-		if err := db.Find(&api).Error; err != nil {
-			if err != gorm.ErrRecordNotFound {
-				log.Println(err)
-				return
-			}
-		}
-
-		if api.Id == "" {
-			data := model.Api{
-				Path:     route.Path,
-				Method:   route.Method,
-				ParentId: &parent.Id,
-			}
-			if err := model.DB.Create(&data).Error; err != nil {
-				log.Println(err)
-				return
-			}
-		} else if api.DeletedAt.Valid {
-			if err := model.DB.Unscoped().Model(&api).Update("deleted_at", nil).Error; err != nil {
-				log.Println(err)
-				return
-			}
-		}
-	}
 	con.Success(ctx, "ok", nil)
-}
-
-func (con ApiController) setParent(path string, p *model.Api) (*model.Api, error) {
-	parent := model.Api{
-		Path: path,
-	}
-	if p != nil {
-		parent.ParentId = &p.Id
-	}
-
-	if err := model.DB.Unscoped().Where(parent).FirstOrCreate(&parent).Error; err != nil {
-		return nil, err
-	}
-	if parent.DeletedAt.Valid {
-		if err := model.DB.Unscoped().Model(&parent).Update("deleted_at", nil).Error; err != nil {
-			return nil, err
-		}
-	}
-
-	return &parent, nil
 }

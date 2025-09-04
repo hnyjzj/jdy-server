21222cef404828703998adfb4f8e95db7574eb1b
waset <waset@foxmail.com>
2025-09-04 11:53:09 +0800

    refactor(api): 优化 API 列表处理逻辑，先清空列表

--- controller/sync/api.go
+++ controller/sync/api.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 type ApiController struct {
@@ -15,6 +16,13 @@ type ApiController struct {
 
 func (con ApiController) List(ctx *gin.Context) {
 	routes := G.Gin.Routes()
+
+	// 清空api表
+	if err := model.DB.Where("1 = 1").Delete(&model.Api{}).Error; err != nil {
+		log.Println(err)
+		return
+	}
+
 	for _, route := range routes {
 		names := strings.Split(route.Path, "/")
 		if names[1] != "api" {
@@ -36,13 +44,38 @@ func (con ApiController) List(ctx *gin.Context) {
 			}
 		}
 
-		var api model.Api
-		api.Path = route.Path
-		api.Method = route.Method
-		api.ParentId = &parent.Id
-		if err := model.DB.Where(model.Api{Path: route.Path, Method: route.Method}).Attrs(api).FirstOrCreate(&api).Error; err != nil {
-			log.Println(err)
-			return
+		var (
+			api model.Api
+		)
+		db := model.DB.Model(&api)
+		db = db.Unscoped()
+		db = db.Where(model.Api{
+			Path:   route.Path,
+			Method: route.Method,
+		})
+
+		if err := db.Find(&api).Error; err != nil {
+			if err != gorm.ErrRecordNotFound {
+				log.Println(err)
+				return
+			}
+		}
+
+		if api.Id == "" {
+			data := model.Api{
+				Path:     route.Path,
+				Method:   route.Method,
+				ParentId: &parent.Id,
+			}
+			if err := model.DB.Create(&data).Error; err != nil {
+				log.Println(err)
+				return
+			}
+		} else if api.DeletedAt.Valid {
+			if err := model.DB.Unscoped().Model(&api).Update("deleted_at", nil).Error; err != nil {
+				log.Println(err)
+				return
+			}
 		}
 	}
 	con.Success(ctx, "ok", nil)
@@ -56,8 +89,13 @@ func (con ApiController) setParent(path string, p *model.Api) (*model.Api, error
 		parent.ParentId = &p.Id
 	}
 
-	if err := model.DB.Where(parent).FirstOrCreate(&parent).Error; err != nil {
+	if err := model.DB.Unscoped().Where(parent).FirstOrCreate(&parent).Error; err != nil {
 		return nil, err
+	}
+	if parent.DeletedAt.Valid {
+		if err := model.DB.Unscoped().Model(&parent).Update("deleted_at", nil).Error; err != nil {
+			return nil, err
+		}
 	}
 
 	return &parent, nil

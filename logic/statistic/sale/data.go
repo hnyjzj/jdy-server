e40a2ef3d9499a2df31541bb88ab6f8a4955a9fa
waset <waset@foxmail.com>
2025-09-08 17:57:20 +0800

    feat(sale): 添加销售统计数据分析功能

--- logic/statistic/sale/data.go
+++ logic/statistic/sale/data.go
@@ -9,7 +9,7 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-type datawLogic struct {
+type dataLogic struct {
 	*StatisticSaleLogic
 	req *DataReq
 
@@ -18,12 +18,17 @@ type datawLogic struct {
 }
 
 type DataRes struct {
-	Overview map[string]any            `json:"overview"` // 总览
-	Trend    map[string]map[string]any `json:"trend"`    // 趋势
+	Overview         map[string]any            `json:"overview"`          // 总览
+	Trend            map[string]map[string]any `json:"trend"`             // 趋势
+	FinishedClass    map[string]any            `json:"finished_class"`    // 成品大类
+	FinishedCategory map[string]map[string]any `json:"finished_category"` // 成品品类
+	OldClass         map[string]any            `json:"old_class"`         // 旧料大类
+	Accessorie       map[string]any            `json:"accessorie"`        // 配件
+	List             map[string]any            `json:"list"`              // 列表
 }
 
 func (l *StatisticSaleLogic) Data(req *DataReq, onlyself bool) (any, error) {
-	logic := datawLogic{
+	logic := dataLogic{
 		StatisticSaleLogic: l,
 		req:                req,
 	}
@@ -36,14 +41,19 @@ func (l *StatisticSaleLogic) Data(req *DataReq, onlyself bool) (any, error) {
 	}
 
 	res := DataRes{
-		Overview: logic.get_overview(),
-		Trend:    logic.get_trend(),
+		Overview:         logic.get_overview(),
+		Trend:            logic.get_trend(),
+		FinishedClass:    logic.get_finished_class(),
+		FinishedCategory: logic.get_finished_category(),
+		OldClass:         logic.get_old_class(),
+		Accessorie:       logic.get_accessorie(),
+		List:             logic.get_list(),
 	}
 
 	return res, nil
 }
 
-func (l *datawLogic) get_sales(onlyself bool) error {
+func (l *dataLogic) get_sales(onlyself bool) error {
 	db := model.DB.Model(&model.OrderSales{})
 	db = db.Where(&model.OrderSales{
 		StoreId: l.req.StoreId,
@@ -72,7 +82,7 @@ func (l *datawLogic) get_sales(onlyself bool) error {
 
 	return nil
 }
-func (l *datawLogic) get_refund(onlyself bool) error {
+func (l *dataLogic) get_refund(onlyself bool) error {
 	db := model.DB.Model(&model.OrderRefund{})
 	db = db.Where(&model.OrderRefund{
 		StoreId: l.req.StoreId,
@@ -91,7 +101,7 @@ func (l *datawLogic) get_refund(onlyself bool) error {
 	return nil
 }
 
-func (l *datawLogic) get_overview() map[string]any {
+func (l *dataLogic) get_overview() map[string]any {
 	data := make(map[string]any)
 
 	if len(l.Sales) == 0 {
@@ -158,7 +168,7 @@ func (l *datawLogic) get_overview() map[string]any {
 	return data
 }
 
-func (l *datawLogic) get_trend() map[string]map[string]any {
+func (l *dataLogic) get_trend() map[string]map[string]any {
 	data := make(map[string]map[string]any)
 
 	now := time.Now()
@@ -247,4 +257,262 @@ func get_date_format(start, end, field time.Time) (string, []string) {
 	}
 
 	return k, list
+}
+
+// 获取成品大类数据
+func (l *dataLogic) get_finished_class() map[string]any {
+	data := make(map[string]any)
+
+	for _, order := range l.Sales {
+		for _, product := range order.Products {
+			if product.Type != enums.ProductTypeFinished {
+				continue
+			}
+			k := product.Finished.Product.Class.String()
+			if k == "" {
+				k = "其他"
+			}
+
+			num_row, ok := data["件数"].(map[string]any)
+			if !ok {
+				num_row = make(map[string]any, 0)
+			}
+			num, ok := num_row[k].(int64)
+			if !ok {
+				num = 0
+			}
+			num = num + 1
+			num_row[k] = num
+			data["件数"] = num_row
+
+			price_row, ok := data["销售额"].(map[string]any)
+			if !ok {
+				price_row = make(map[string]any, 0)
+			}
+			price, ok := price_row[k].(decimal.Decimal)
+			if !ok {
+				price = decimal.NewFromInt(0)
+			}
+			price = price.Add(product.Finished.Price)
+			price_row[k] = price
+			data["销售额"] = price_row
+		}
+	}
+
+	return data
+}
+
+// 获取成品品类数据
+func (l *dataLogic) get_finished_category() map[string]map[string]any {
+
+	data := make(map[string]map[string]any)
+
+	for _, class := range enums.ProductClassFinishedMap {
+		data[class] = map[string]any{
+			"件数": map[string]any{},
+			"金重": map[string]any{},
+			"标价": map[string]any{},
+		}
+	}
+
+	for _, order := range l.Sales {
+		for _, product := range order.Products {
+			c := product.Finished.Product.Class.String()
+			if c == "" {
+				c = "其他"
+			}
+			k := product.Finished.Product.Category.String()
+			if k == "" {
+				k = "其他"
+			}
+
+			num_item, ok := data[c]["件数"].(map[string]any)
+			if !ok {
+				num_item = make(map[string]any, 0)
+			}
+			num, ok := num_item[k].(decimal.Decimal)
+			if !ok {
+				num = decimal.Zero
+			}
+			num = num.Add(decimal.NewFromInt(1))
+			num_item[k] = num
+			data[c]["件数"] = num_item
+
+			price_item, ok := data[c]["销售额"].(map[string]any)
+			if !ok {
+				price_item = make(map[string]any, 0)
+			}
+			price, ok := price_item[k].(decimal.Decimal)
+			if !ok {
+				price = decimal.Zero
+			}
+			price = price.Add(product.Finished.Price)
+			price_item[k] = price
+			data[c]["销售额"] = price_item
+
+			weight_item, ok := data[c]["金重"].(map[string]any)
+			if !ok {
+				weight_item = make(map[string]any, 0)
+			}
+			weight, ok := weight_item[k].(decimal.Decimal)
+			if !ok {
+				weight = decimal.Zero
+			}
+			weight = weight.Add(product.Finished.Product.WeightMetal)
+			weight_item[k] = weight
+			data[c]["金重"] = weight_item
+		}
+	}
+
+	return data
+}
+
+// 获取旧料大类数据
+func (l *dataLogic) get_old_class() map[string]any {
+	data := make(map[string]any)
+
+	for _, order := range l.Sales {
+		for _, product := range order.Products {
+			if product.Type != enums.ProductTypeOld {
+				continue
+			}
+			k := product.Old.Product.Class.String()
+			if k == "" {
+				k = "其他"
+			}
+
+			price_row, ok := data["抵值"].(map[string]any)
+			if !ok {
+				price_row = make(map[string]any, 0)
+			}
+			price, ok := price_row[k].(decimal.Decimal)
+			if !ok {
+				price = decimal.NewFromInt(0)
+			}
+			price = price.Add(product.Old.RecyclePrice)
+			price_row[k] = price
+			data["抵值"] = price_row
+
+			weight_item, ok := data["金重"].(map[string]any)
+			if !ok {
+				weight_item = make(map[string]any, 0)
+			}
+			weight, ok := weight_item[k].(decimal.Decimal)
+			if !ok {
+				weight = decimal.Zero
+			}
+			weight = weight.Add(product.Old.WeightMetal)
+			weight_item[k] = weight
+			data["金重"] = weight_item
+
+			num_row, ok := data["件数"].(map[string]any)
+			if !ok {
+				num_row = make(map[string]any, 0)
+			}
+			num, ok := num_row[k].(int64)
+			if !ok {
+				num = 0
+			}
+			num = num + 1
+			num_row[k] = num
+			data["件数"] = num_row
+
+		}
+	}
+
+	return data
+}
+
+// 获取配件数据
+func (l *dataLogic) get_accessorie() map[string]any {
+	data := make(map[string]any)
+
+	for _, order := range l.Sales {
+		for _, product := range order.Products {
+			if product.Type != enums.ProductTypeAccessorie {
+				continue
+			}
+			k := product.Accessorie.Product.Name + "(" + product.Accessorie.Product.Type.String() + ")"
+
+			price_row, ok := data["销售额"].(map[string]any)
+			if !ok {
+				price_row = make(map[string]any, 0)
+			}
+			price, ok := price_row[k].(decimal.Decimal)
+			if !ok {
+				price = decimal.NewFromInt(0)
+			}
+			price = price.Add(product.Accessorie.Price)
+			price_row[k] = price
+			data["销售额"] = price_row
+
+			num_row, ok := data["件数"].(map[string]any)
+			if !ok {
+				num_row = make(map[string]any, 0)
+			}
+			num, ok := num_row[k].(int64)
+			if !ok {
+				num = 0
+			}
+			num = num + product.Accessorie.Quantity
+			num_row[k] = num
+			data["件数"] = num_row
+
+		}
+	}
+
+	return data
+}
+
+// 获取列表
+func (l *dataLogic) get_list() map[string]any {
+	data := make(map[string]any)
+
+	for _, order := range l.Sales {
+		for _, clerk := range order.Clerks {
+			k := clerk.Salesman.Nickname
+
+			row, ok := data[k].(map[string]any)
+			if !ok {
+				row = make(map[string]any, 0)
+			}
+			finished_price, ok := row["成品销售额"].(decimal.Decimal)
+			if !ok {
+				finished_price = decimal.NewFromInt(0)
+			}
+
+			accessorie_price, ok := row["配件销售额"].(decimal.Decimal)
+			if !ok {
+				accessorie_price = decimal.NewFromInt(0)
+			}
+
+			finished_num, ok := row["成品件数"].(int64)
+			if !ok {
+				finished_num = 0
+			}
+
+			accessorie_num, ok := row["配件件数"].(int64)
+			if !ok {
+				accessorie_num = 0
+			}
+
+			for _, product := range order.Products {
+				switch product.Type {
+				case enums.ProductTypeFinished:
+					finished_price = finished_price.Add(product.Finished.Price.Mul(clerk.PerformanceRate).Div(decimal.NewFromFloat(100)))
+					finished_num = finished_num + 1
+				case enums.ProductTypeAccessorie:
+					accessorie_price = accessorie_price.Add(product.Accessorie.Price.Mul(clerk.PerformanceRate).Div(decimal.NewFromFloat(100)))
+					accessorie_num = accessorie_num + product.Accessorie.Quantity
+				}
+			}
+			row["成品销售额"] = finished_price
+			row["配件销售额"] = accessorie_price
+			row["成品件数"] = finished_num
+			row["配件件数"] = accessorie_num
+			data[k] = row
+		}
+	}
+
+	return data
 }

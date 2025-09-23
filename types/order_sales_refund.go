8a34483cf940597ed9d187bb178d31e10b683daf
waset <waset@foxmail.com>
2025-09-23 17:30:49 +0800

    feat(order): 退货单添加货品类型查询条件

--- types/order_sales_refund.go
+++ types/order_sales_refund.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"jdy/enums"
 	"time"
 )
 
@@ -9,8 +10,9 @@ type OrderSalesRefundWhere struct {
 	StoreId string `json:"store_id" label:"门店" find:"false" sort:"3" type:"string" input:"search" required:"true"` // 门店
 	Phone   string `json:"phone" label:"会员(手机号)" find:"true" create:"true" sort:"3" type:"string" input:"text"`    // 会员
 
-	Code string `json:"code" label:"货品条码" find:"true" sort:"5" type:"string" input:"text"` // 货品条码
-	Name string `json:"name" label:"货品名称" find:"true" sort:"6" type:"string" input:"text"` // 货品名称
+	Type enums.ProductType `json:"type" label:"货品类型" find:"true" sort:"4"  type:"number" input:"select" preset:"typeMap"` // 货品类型
+	Code string            `json:"code" label:"货品条码" find:"true" sort:"5" type:"string" input:"text"`                     // 货品条码
+	Name string            `json:"name" label:"货品名称" find:"true" sort:"6" type:"string" input:"text"`                     // 货品名称
 
 	StartDate *time.Time `json:"start_date" label:"开始日期" find:"true" sort:"9" type:"string" input:"date"` // 开始日期
 	EndDate   *time.Time `json:"end_date" label:"结束日期" find:"true" sort:"10" type:"string" input:"date"`  // 结束日期
